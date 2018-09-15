339325b850c94ba6c71b10b20312e131de057295
Patric Forsgard <patric@tasteful.se>
2018-09-15 19:26:43 +0200

    Remove the extension method that take in the IServiceCollection, building multiple service providers only for the setup cant be a best practice and if a user want to do that outisde AM.Collection.EFCore they can but it should not be as a default extension method.

--- src/AutoMapper.Collection.EntityFrameworkCore/MapperConfigurationExpressionExtensions.cs
+++ src/AutoMapper.Collection.EntityFrameworkCore/MapperConfigurationExpressionExtensions.cs
@@ -24,20 +24,6 @@ namespace AutoMapper
 
         /// <summary>
         /// Generates and adds property maps based on the primary keys for the given <see cref="DbContext"/>. This is done by resolving an
-        /// instance of the <see cref="DbContext"/> using a temporary <see cref="IServiceProvider"/> based on the <see cref="IServiceCollection"/>.
-        /// This method is generally called from <see cref="IServiceCollection"/>.AddAutoMapper().
-        /// </summary>
-        public static void UseEntityFrameworkCoreModel<TContext>(this IMapperConfigurationExpression config, IServiceCollection services)
-            where TContext : DbContext
-        {
-            using (var serviceProvider = services.BuildServiceProvider())
-            {
-                config.UseEntityFrameworkCoreModel<TContext>(serviceProvider);
-            }
-        }
-
-        /// <summary>
-        /// Generates and adds property maps based on the primary keys for the given <see cref="DbContext"/>. This is done by resolving an
         /// instance of the <see cref="DbContext"/> using the <see cref="IServiceProvider"/>. This method is generally used when you are configuring
         /// AutoMapper using static initialization via <see cref="Mapper.Initialize(Action{IMapperConfigurationExpression})"/>.
         /// </summary>
